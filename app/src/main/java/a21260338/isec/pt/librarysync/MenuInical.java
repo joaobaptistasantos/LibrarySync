ec21fb6e2712ae0b089268bbb3b579befe39433e
joaosantos <jocatoca3@gmail.com>
2018-11-27 20:13:15 +0000

    Criacao do Menu Registar

--- app/src/main/java/a21260338/isec/pt/librarysync/MenuInical.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/MenuInical.java
@@ -1,7 +1,9 @@
 package a21260338.isec.pt.librarysync;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuInical extends Activity {
 
@@ -9,5 +11,11 @@ public class MenuInical extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_inical);
+
+    }
+
+    public void registo(View v){
+        Intent intent = new Intent(this, MenuRegistar.class);
+        startActivity(intent);
     }
 }
