543bbdcf34b2852b5e5bc012dec215f51cb4df2e
joaosantos <jocatoca3@gmail.com>
2018-11-28 01:03:33 +0000

    Add onClick para os Dialogs sobre nas vistas do Menu Principal de Docentes e Recepcionista e nas Definicoes de Conta do Utilizador no Sobre a Aplicacao

--- app/src/main/java/a21260338/isec/pt/librarysync/MenuPrincipalRecepcionista.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/MenuPrincipalRecepcionista.java
@@ -1,13 +1,41 @@
 package a21260338.isec.pt.librarysync;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MenuPrincipalRecepcionista extends Activity {
+
+    private AlertDialog sobre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal_recepcionista);
+    }
+
+    public void sobre(View v){
+        // LayoutInflater é utilizado para inflar o layout numa view
+        // Pegamos nessa instancia da classe
+        LayoutInflater li = getLayoutInflater();
+
+        // Inflamos o layout sobre.xml na view
+        View view = li.inflate(R.layout.sobre, null);
+        // Definimos para o botão do layout um clickListener
+        view.findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Desfaz o alerta
+                sobre.dismiss();
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Sobre")
+                .setView(view);
+
+        sobre = builder.create();
+        sobre.show();
     }
 }
