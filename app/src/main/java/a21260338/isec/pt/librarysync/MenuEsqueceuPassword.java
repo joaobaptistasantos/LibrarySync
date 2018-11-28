c14cef3888b79aa90d02b3de57f7e56c7bad6bb6
joaosantos <jocatoca3@gmail.com>
2018-11-28 00:50:48 +0000

    Add onClick Esqueceu Password to Login Add onClick Esqueceu Password to Registar

--- app/src/main/java/a21260338/isec/pt/librarysync/MenuEsqueceuPassword.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/MenuEsqueceuPassword.java
@@ -1,7 +1,9 @@
 package a21260338.isec.pt.librarysync;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuEsqueceuPassword extends Activity {
 
@@ -9,5 +11,14 @@ public class MenuEsqueceuPassword extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_esqueceu_password);
+    }
+
+    public void onRegistar(View v){
+        Intent intent = new Intent(this, MenuRegistar.class);
+        startActivity(intent);
+    }
+
+    public void onLogin(View v){
+        finish();
     }
 }
