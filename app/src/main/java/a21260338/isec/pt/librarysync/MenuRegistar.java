ea8cfa02a146ef44e6f754317581ad5b00ff9f75
joaosantos <jocatoca3@gmail.com>
2018-11-28 00:46:04 +0000

    Add onClick Registar to Login

--- app/src/main/java/a21260338/isec/pt/librarysync/MenuRegistar.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/MenuRegistar.java
@@ -2,6 +2,7 @@ package a21260338.isec.pt.librarysync;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuRegistar extends Activity {
 
@@ -9,5 +10,9 @@ public class MenuRegistar extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_registar);
+    }
+
+    public void toLogin(View v){
+        finish();
     }
 }
