466762f01dccba3884247f78763c72b3a3349566
joaosantos <jocatoca3@gmail.com>
2018-11-28 00:53:51 +0000

    Add onClick Menu Principal Utilizador to Definicoes Add onClick Menu Principal Utilizador to Sair

--- app/src/main/java/a21260338/isec/pt/librarysync/MenuPrincipal.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/MenuPrincipal.java
@@ -1,7 +1,9 @@
 package a21260338.isec.pt.librarysync;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuPrincipal extends Activity {
 
@@ -9,5 +11,14 @@ public class MenuPrincipal extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+    }
+
+    public void exit(View v) {
+        finish();
+    }
+
+    public void definicoes(View v){
+        Intent intent = new Intent(this, MenuDefinicoes.class);
+        startActivity(intent);
     }
 }
