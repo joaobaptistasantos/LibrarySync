45cbfc9e2d9b25d364089d1586ea0f2d53a0fa3c
joaosantos <jocatoca3@gmail.com>
2018-11-28 04:26:34 +0000

    Insercao da Classe Aluno que deriva de Utilizador

---
+++ app/src/main/java/Aluno.java
@@ -0,0 +1,20 @@
+public class Aluno extends Utilizador {
+
+    int nrAluno;
+
+    public Aluno(String Email, String Password) {
+        super(Email, Password);
+    }
+
+    public int getNrAluno() {
+        return nrAluno;
+    }
+
+    public void setNrAluno(int nrAluno) {
+        this.nrAluno = nrAluno;
+    }
+
+    public void editaInformacao(String Email, String Password){
+
+    }
+}
