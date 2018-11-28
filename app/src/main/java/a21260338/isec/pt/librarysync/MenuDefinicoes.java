16053c5961ea25abed8056f0246edec072ce5d9c
joaosantos <jocatoca3@gmail.com>
2018-11-28 01:06:05 +0000

    Add onClick para o Dialog de aviso de cancelamento de conta na vista de Definicoes de Conta do Utilizador no Cancelar Conta

--- app/src/main/java/a21260338/isec/pt/librarysync/MenuDefinicoes.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/MenuDefinicoes.java
@@ -2,18 +2,47 @@ package a21260338.isec.pt.librarysync;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 public class MenuDefinicoes extends Activity {
 
     private AlertDialog sobre;
+    private AlertDialog aviso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_definicoes);
+    }
+
+    public void cancelarConta(View v){
+        //Cria o gerador do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Aviso");
+        //define a mensagem
+        builder.setMessage("Tem a certeza que pretende cancelar a sua conta?");
+        //define um botão como positivo
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                // para implementar
+            }
+        });
+        //define um botão como negativo.
+        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                // para implementar
+            }
+        });
+
+        //cria o AlertDialog
+        aviso = builder.create();
+        //Exibe
+        aviso.show();
     }
 
     public void sobre(View v){
