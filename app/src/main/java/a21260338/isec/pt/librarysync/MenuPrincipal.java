45569f2141ccc5f9bb4abf6f861fa6fc2b49713b
joaosantos <jocatoca3@gmail.com>
2018-11-28 00:13:08 +0000

    Criacao do Menu Principal do Utilizador

---
+++ app/src/main/java/a21260338/isec/pt/librarysync/MenuPrincipal.java
@@ -0,0 +1,13 @@
+package a21260338.isec.pt.librarysync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuPrincipal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
