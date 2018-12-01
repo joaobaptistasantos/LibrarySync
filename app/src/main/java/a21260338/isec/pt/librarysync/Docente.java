27b4c9994886ea2ff10c1a581404792a63d179ef
joaosantos <jocatoca3@gmail.com>
2018-12-01 16:10:18 +0000

    Ajustes nas classes (metodos e atributos) para baterem certo com o Diagrama de Classes

--- app/src/main/java/a21260338/isec/pt/librarysync/Docente.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/Docente.java
@@ -2,25 +2,19 @@ package a21260338.isec.pt.librarysync;
 
 public class Docente extends Utilizador {
 
-    int nrDocente;
+    private static int contadorDocentes = 1;
+    private int nrDocente;
 
-    public Docente(String Email, String Password) {
-        super(Email, Password);
+    public Docente(String email, String password) {
+        super(email, password);
+        nrDocente = contadorDocentes++;
     }
 
     public int getNrDocente() {
         return nrDocente;
     }
 
-    public void setNrDocente(int nrDocente) {
-        this.nrDocente = nrDocente;
-    }
-
     public String imprimeEstatisticas(){
         return "";
-    }
-
-    public void editaInformacao(String email, String password){
-
     }
 }
