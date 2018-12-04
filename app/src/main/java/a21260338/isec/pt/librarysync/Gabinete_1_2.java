5e543b26d88419a07ca0bccc0cae7436943058d9
AmadeusAlves <34137136+AmadeusAlves@users.noreply.github.com>
2018-12-04 20:22:13 +0000

    Menu ReservaGabinetes + escolher hora

---
+++ app/src/main/java/a21260338/isec/pt/librarysync/Gabinete_1_2.java
@@ -0,0 +1,29 @@
+package a21260338.isec.pt.librarysync;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Gabinete_1_2 extends Fragment {
+
+
+    public Gabinete_1_2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gabinete_1_2, container, false);
+    }
+
+}
--- app/src/main/java/a21260338/isec/pt/librarysync/MenuReservarGabinete.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/MenuReservarGabinete.java
@@ -3,8 +3,13 @@ package a21260338.isec.pt.librarysync;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,6 +29,11 @@ public class MenuReservarGabinete extends Activity {
     private Utilizadores utilizadores;
     private Utilizador ativo;
     private DatePicker picker;
+    private FragmentTransaction transaction;
+    private Fragment newFragment;
+    private int grupoGabinetesAtual = 1;
+    private String turno = null;
+    private View turnoAnterior = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,12 @@ public class MenuReservarGabinete extends Activity {
 
         TextView dataTV = (TextView) findViewById(R.id.dataText_MenuReservarGabinete);
         dataTV.setText(new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime()));
+
+        newFragment = new Gabinete_1_2();
+        transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment, newFragment);
+        transaction.commit();
+
     }
 
     public void back(View v){
@@ -79,5 +95,49 @@ public class MenuReservarGabinete extends Activity {
 
     public void concluido(View v){
         finish();
+    }
+
+    public void gabinetesSeguintes(View view) {
+        if(grupoGabinetesAtual == 1){
+        grupoGabinetesAtual++;
+        newFragment = new Gabinete_3_4();
+        transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment, newFragment);
+        transaction.commit();
+        }else
+        if(grupoGabinetesAtual == 2){
+            grupoGabinetesAtual++;
+            newFragment = new Gabinete_5_6();
+            transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment, newFragment);
+            transaction.commit();
+        }else return;
+    }
+
+    public void gabinetesAnteriores(View view) {
+        if(grupoGabinetesAtual == 3){
+            grupoGabinetesAtual--;
+            newFragment = new Gabinete_3_4();
+            transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment, newFragment);
+            transaction.commit();
+        }else
+        if(grupoGabinetesAtual == 2){
+            grupoGabinetesAtual--;
+            newFragment = new Gabinete_1_2();
+            transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment, newFragment);
+            transaction.commit();
+        }else return;
+    }
+
+    public void horaReserva(View view) {
+        if(turnoAnterior!=null){
+            turnoAnterior.getBackground().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.DARKEN);
+        }
+        turno = getResources().getResourceEntryName(view.getId());
+        view.getBackground().setColorFilter(Color.parseColor("#00FF00"), PorterDuff.Mode.DARKEN);
+        Log.d("Useres: ", "Gabinetes: " + grupoGabinetesAtual + "turno: " + turno);
+        turnoAnterior = view;
     }
 }
