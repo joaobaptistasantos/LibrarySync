5273556c87d4d62c1655bc87a0fd3f66e7a18db8
João Monteiro <joao.monteiro95@hotmail.com>
2018-12-06 00:30:41 +0000

    Controlador implements Serializable

--- app/src/main/java/a21260338/isec/pt/librarysync/Controlador.java
+++ app/src/main/java/a21260338/isec/pt/librarysync/Controlador.java
@@ -1,12 +1,20 @@
 package a21260338.isec.pt.librarysync;
 
+import android.util.Log;
+
+import java.io.Serializable;
 import java.util.List;
 import java.util.ArrayList;
 
-public abstract class Controlador<T extends Modelo> {
+public abstract class Controlador<T extends Modelo> implements Serializable {
     private List<T> listData = new ArrayList<T>();
     protected Controlador(ArrayList<T> listData){
-        this.listData = listData;
+        try {
+            this.listData = listData;
+        } catch (java.lang.NullPointerException e) {
+            Log.d("NullPointerException", e.toString());
+        }
+
     }
     protected List<T> GetListData(){
         return listData;
